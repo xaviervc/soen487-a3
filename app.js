f75def502e3a5ebe7a0037d1110110d27d2783a1
Ryan <rytay123@gmail.com>
2020-04-15 03:06:03 -0400

    added json support for sending coordinates

--- app.js
+++ app.js
@@ -3,15 +3,56 @@ const app = express();
 const Places = require('./models/Places');
 
 app.use(express.urlencoded({extended: true}));
+app.use(express.json()) 
+
+
+const parseJson = (json) => {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve(JSON.parse(json))
+      })
+    })
+  }
 
 //Routes
 app.get('/', (req, res) => {
     res.send(export_places);
 });
 
+app.post('/jsonLocation',(req,res)=> {
+    
+    jsonData = req.body
+    
+    longitude = parseFloat(jsonData.longitude)
+    latitude = parseFloat(jsonData.latitude)
+    max = 30
+    min = 10
+    coordinate = [longitude,latitude]
+    console.log(coordinate)
+
+    place = new Places(max,min)
+    responseBody = place.near(coordinate).then(
+        result => {
+            console.log(result)
+            res.send(result)
+        }
+   
+        
+    )
+    
+    
+    
+    
+})
+
+
+
+
 app.post('/', (req, res) =>{
     console.log(req.body);
     res.status(200).end();
 })
+
+
 
 app.listen(3000);
